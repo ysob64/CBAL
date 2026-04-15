1ee858fb5342d26e5ceeee2a2e9a9d941a3accfd
ysob64 <168114780+ysob64@users.noreply.github.com>
2026-04-15 13:49:10 +0200

    Update stringlib.h
    added s_empty()

--- hroot/stringlib.h
+++ hroot/stringlib.h
@@ -6,4 +6,5 @@ int s_Lenght(const char *s);
 void s_Merge(char *buffer,const char *s1,const char *s2);
 int s_toInt(const char *s1);
 char *s_copy(const char *s1);
+int s_Empty(char *s);
 bool IsDesktopShortcut(const char *s1);
